9181a16a17caa13b15aed5f9577bff5314bcb003
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2020-01-20 00:00:46 +0530

    File handling - Reading of file working
    Code not following DRY principle.
    Constant pointer is used to remove warning.

--- customCrypto.c
+++ customCrypto.c
@@ -1,28 +1,50 @@
 #include <stdio.h>
 #include <string.h>
 
-int encrypt() { return 0; }
-
-int decrypt() { return 0; }
-
-void openFile(char *filename) 
+int encrypt(const char *filename)
 {
     FILE *fp;
     fp = fopen(filename, "r+");
 
     if (fp == NULL)
     {
-        printf("File does not exist / unable to open file");
+        printf("File does not exist / unable to open file\n");
     }
     else
     {
-        printf("File exists");
-    }
-    
-    fclose(fp);
+        printf("File found\n");
 
+        char dataToBeRead[50]; 
+        // using fgets() method 
+        while( fgets ( dataToBeRead, 50, fp ) != NULL ) 
+        { 
+            // Print the dataToBeRead  
+            printf("%s" , dataToBeRead) ; 
+         } 
+
+        fclose(fp);
+    }
+
+    return 0;
 }
 
+int decrypt(const char *filename)
+{
+    FILE *fp;
+    fp = fopen(filename, "r+");
+
+    if (fp == NULL)
+    {
+        printf("File does not exist / unable to open file\n");
+    }
+    else
+    {
+        printf("File found\n");
+        fclose(fp);
+    }
+
+    return 0;
+}
 
 int main(int argc, char const *argv[])
 {
@@ -33,22 +55,19 @@ int main(int argc, char const *argv[])
     case 2:
         printf("You have selected no flag\n");
         printf("By default encrypt flag will run\n");
-        openFile(argv[2]);
-        encrypt();
+        encrypt(argv[2]);
         break;
 
     case 3:
         if (strcmp(argv[1], "-encrypt") == 0)
         {
             printf("You have selected to encrypt file...\n");
-            openFile(argv[2]);
-            encrypt();
+            encrypt(argv[2]);
         }
         else if (strcmp(argv[1], "-decrypt") == 0)
         {
             printf("You have selected to decrypt file...\n");
-            openFile(argv[2]);
-            decrypt();
+            decrypt(argv[2]);
         }
         else
         {
