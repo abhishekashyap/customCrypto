ce7fe0a7599a6765377f548fbdb35648e59e2ab8
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2020-01-19 23:41:56 +0530

    File handling: opening of file working

--- customCrypto.c
+++ customCrypto.c
@@ -3,26 +3,52 @@
 
 int encrypt() { return 0; }
 
+int decrypt() { return 0; }
+
+void openFile(char *filename) 
+{
+    FILE *fp;
+    fp = fopen(filename, "r+");
+
+    if (fp == NULL)
+    {
+        printf("File does not exist / unable to open file");
+    }
+    else
+    {
+        printf("File exists");
+    }
+    
+    fclose(fp);
+
+}
+
+
 int main(int argc, char const *argv[])
 {
     printf("You have entered %d arguments\n", argc);
 
     switch (argc)
     {
-    case 1:
+    case 2:
         printf("You have selected no flag\n");
         printf("By default encrypt flag will run\n");
+        openFile(argv[2]);
         encrypt();
         break;
 
-    case 2:
+    case 3:
         if (strcmp(argv[1], "-encrypt") == 0)
         {
             printf("You have selected to encrypt file...\n");
+            openFile(argv[2]);
+            encrypt();
         }
         else if (strcmp(argv[1], "-decrypt") == 0)
         {
             printf("You have selected to decrypt file...\n");
+            openFile(argv[2]);
+            decrypt();
         }
         else
         {
@@ -33,14 +59,18 @@ int main(int argc, char const *argv[])
         break;
 
     default:
-        printf("Please enter valid command\n");
+        printf("Please enter valid flags and arguments\n");
+        printf("Syntax: customCrypto [-flags] filename\n");
         break;
     }
 
+    /*
+    // Printing CLI args
     for (int i = 0; i < argc; i++)
     {
         printf("%s\n", argv[i]);
     }
+    */
 
     return 0;
 }
